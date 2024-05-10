5acdfdc060b1cdc2cef2cb6c3d0d6fb39dcb4ba7
petsamuel <petsamuel4@gmail.com>
2024-05-10 11:49:30 +0100

    modified

--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -3,23 +3,25 @@ import React,{Fragment, useState} from 'react'
 import { Header } from "../component/Header";
 import { Footer } from "../component/Footer";
 import {HeadingText} from "../component/Heading"
+import { FaPlus, FaMinus } from "react-icons/fa6";
 import {faq} from "@/app/static-data"
 const Page = () => {
     const [active, setActive] =useState<number>()
   return (
     <Fragment>
       <Header />
-      <main className='container my-10'>
+      <main className='container my-8 '>
         <div className='flex items-center justify-center'>
           <HeadingText title='Frequently Asked Questions' />
         </div>
 
         <div className=''>
-          <div className='my-4'>
+          <div className='my-5'>
             {faq.map((val, index) => (
               <div key={index} className='flex flex-col  gap-4 ' >
-                <div className='flex justify-between items-center '>
-                  <p className=' font-semibold bg-gray-50 p-5 cursor-pointer w-full mb-3 rounded'>{val.question} </p>
+                <div className='flex justify-between items-center bg-gray-50 p-5 mb-3 w-full rounded'>
+                  <p className=' font-semibold  cursor-pointer '>{val.question} </p>
+                  {active === index ? <FaMinus/> :<FaPlus/>}
                 </div>
                 {active === index ? <p>{val.answer}</p> : null}
               </div>
