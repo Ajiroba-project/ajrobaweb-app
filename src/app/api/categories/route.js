24c9c30cc38e1053ce8338396cfad7fbbb59fdf4
femakin <santosfemakin@yahoo.com>
2024-05-23 16:03:20 +0100

    modified categories

--- src/app/api/categories/route.js
+++ src/app/api/categories/route.js
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 
 export async function GET(req, res) {
     try {
-        return NextResponse.json({});
+        return NextResponse.json({ data: 'data' });
     } catch (error) {
         return NextResponse.error(new Error('Internal Server Error'));
     }
