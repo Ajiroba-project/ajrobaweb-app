684a8ebdc4a6649aaa469d919976ef89725f80d2
femakin <santosfemakin@yahoo.com>
2024-06-01 16:25:25 +0100

    Auth cleanup and correction fixes

--- src/app/component/AuthHero.tsx
+++ src/app/component/AuthHero.tsx
@@ -8,7 +8,7 @@ interface AuthHeroProps {
 function AuthHero({ title, menu }: AuthHeroProps) {
     return (
         <section className='flex justify-center items-center flex-col' >
-            <h1 className='xl:text-2xl 2xl:text-2xl md:text-2xl text-base font-bold leading-tight tracking-tight text-left '  >{title}</h1>
+            <h1 className='xl:text-2xl 2xl:text-2xl md:text-2xl text-base font-Poppins text-[#111111] leading-tight tracking-tight text-left '  >{title}</h1>
             <p className=' mt-4 text-sm font-normal leading-6 text-center xl:w-1/3 w-auto  text-[#353131]' >{menu}</p>
         </section>
     );
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next'
-import { Poppins } from 'next/font/google'
+import { Poppins, Inter } from 'next/font/google'
 import './globals.css'
 import RQProviders from '@/utils/provider'
 // import { useExcludeHeader } from '@/hooks/useExcludeHeader'
@@ -7,6 +7,11 @@ import RQProviders from '@/utils/provider'
 // import { Footer } from './component/Footer'
 
 const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '900']
+})
+
+const inter = Inter({
   subsets: ['latin'],
   weight: ['400', '500', '600', '900']
 })
@@ -21,13 +26,13 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode
 }>) {
-  
+
 
   return (
     <html lang='en'>
       <RQProviders>
         {/* header */}
-      <body className={poppins.className}>{children}</body>
+        <body className={`${poppins.className} ${inter.className}`}>{children}</body>
       </RQProviders>
       {/* footer */}
     </html>
