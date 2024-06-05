ceff32c5d28a7b4e755c47be50e2f2141157ce58
femakin <santosfemakin@yahoo.com>
2024-06-05 10:45:33 +0100

    fixes

--- src/app/api/signin/route.js
+++ src/app/api/signin/route.js
@@ -78,7 +78,9 @@ export async function POST(request) {
 
     const responseData = { data, status };
 
-    // console.log(responseData, 'resddddd')
+    /*  console.log(data, 'ressssss')
+ 
+         / console.log(responseData, 'resddddd') */
 
     return NextResponse.json(responseData);
 }
