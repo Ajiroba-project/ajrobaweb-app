2d36eea05c7ae410c2ca9165a89bbfc368362014
femakin <santosfemakin@yahoo.com>
2024-08-09 17:39:42 +0100

    product_details_

--- src/app/profile/components/ModalProfile.tsx
+++ src/app/profile/components/ModalProfile.tsx
@@ -1,14 +1,27 @@
 import Image from 'next/image';
-import React from 'react';
+import React, { ReactNode } from 'react';
 
-export const ModalProfile = ({
+interface ModalProfileProps {
+  isOpen: boolean;
+  title?: string;
+  icon?: string;
+  children?: ReactNode;
+  buttoncount?: number;
+  buttontext?: string;
+  buttonclass?: string;
+  buttontype?: 'button' | 'submit' | 'reset';
+  handleEvent?: () => void;
+  onClose?: () => void;
+}
+
+export const ModalProfile: React.FC<ModalProfileProps> = ({
   isOpen,
-  onClose,
   title,
   icon,
   children,
+  onClose,
   buttoncount = 1,
-  buttontext = "Close",
+  buttontext = 'Close',
   buttonclass = '',
   buttontype = 'button',
   handleEvent,
@@ -16,29 +29,25 @@ export const ModalProfile = ({
   if (!isOpen) return null;
 
   return (
-
     <>
-    {
-        isOpen &&
-  <section className='fixed left-0 top-0 z-50 flex h-full w-screen items-center justify-center bg-black bg-opacity-50 p-4'>
-      <div className='mb-8 flex h-auto flex-col items-center justify-center gap-6 rounded-md bg-white p-8'>
-        {icon && <Image src={icon} alt="icon" />}
-        {title && <h2 className='text-xl font-semibold'>{title}</h2>}
-        {children && <div className="my-4">{children}</div>}
-      {/*   <div className='mt-5 flex justify-center gap-4'>
-          <button
-            className={`rounded-md bg-[#F25E26] p-2 text-white ${buttonclass}`}
-            type={buttontype}
-            onClick={handleEvent || onClose}
-          >
-            {buttontext}
-          </button>
-        </div> */}
-      </div>
-    </section>
-
-    }
+      {isOpen && (
+        <section className="fixed left-0 top-0 z-50 flex h-full w-screen items-center justify-center bg-black bg-opacity-50 p-4">
+          <div className="mb-8 flex h-auto flex-col items-center justify-center gap-6 rounded-md bg-white p-8">
+            {icon && <Image src={icon} alt="icon" />}
+            {title && <h2 className="text-xl font-semibold">{title}</h2>}
+            {children && <div className="my-4">{children}</div>}
+            {/* <div className='mt-5 flex justify-center gap-4'>
+              <button
+                className={`rounded-md bg-[#F25E26] p-2 text-white ${buttonclass}`}
+                type={buttontype}
+                onClick={handleEvent || onClose}
+              >
+                {buttontext}
+              </button>
+            </div> */}
+          </div>
+        </section>
+      )}
     </>
-
   );
 };
