de5b802c97006a57a66cce27983911c43ec170b1
femakin <santosfemakin@yahoo.com>
2024-08-13 07:52:33 +0100

    fixes on push

--- src/app/purchaseorder/page.jsx
+++ src/app/purchaseorder/page.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { Suspense } from "react";
 import { Header } from "../component/Header";
 import { Profile } from "../../app/profile/components/Profile";
 import { useAuthStore, userProfile } from "@/store/store";
