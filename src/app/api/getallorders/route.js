ac27b43a40edb3641f7a231084688e6d1b5f89e8
femakin <santosfemakin@yahoo.com>
2024-08-13 20:26:27 +0100

    fixes

--- src/app/api/getallorders/route.js
+++ src/app/api/getallorders/route.js
@@ -1,5 +1,7 @@
 import { NextResponse } from "next/server";
 
+export const dynamic = 'force-dynamic';
+
 export async function GET(request) {
     try {
         // Extract the token from the query parameters or headers
