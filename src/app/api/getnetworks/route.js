81d774ac9c41484bdf20e5d4d783984f4d2fb073
femakin <santosfemakin@yahoo.com>
2025-02-10 11:41:08 +0100

    payment page

---
+++ src/app/api/getnetworks/route.js
@@ -0,0 +1,52 @@
+import { NextResponse } from "next/server";
+import { cookies } from "next/headers";
+
+export async function POST(request) {
+    try {
+        const body = await request.json();
+        const cacheBuster = `cache=${Date.now()}`;
+
+        /*        const token_ = request.headers.get('authorization')?.replace('Token ', ''); */
+
+        const cookieStore = cookies()
+        const token = cookieStore.get('token')
+
+        console.log(token_, 'token_');
+
+        /*     console.log(body, 'bodyyy');
+            console.log(body.payload);
+            console.log(body.tkn); */
+
+        // Create headers object dynamically
+
+        // console.log(body, 'bodyyy');
+        const headers = {
+            "Content-Type": "application/json",
+        };
+
+        // Conditionally add Authorization header
+        if (token.value) {
+            headers['Authorization'] = `token ${token.value}`;
+        }
+
+        const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/commerce/cart/?${cacheBuster}`, {
+            method: "GET",
+            maxBodyLength: Infinity,
+            headers: headers, // Use the dynamically created headers object
+            body: JSON.stringify({
+                /*   "product_id": body.payload.product_id,
+                  "quantity": body.payload.quantity,
+                  "session_key": body.payload.session_key, */
+                "session_key": 'body.payload.session_key',
+            }),
+        });
+
+        const data = await res.json();
+        const status = res.status;
+
+        return NextResponse.json({ data, status });
+    } catch (error) {
+        console.error('Error processing request:', error.message);
+        return NextResponse.error(new Error('Internal Server Error'));
+    }
+}
--- src/app/recharge/components/AirtimeDetails.tsx
+++ src/app/recharge/components/AirtimeDetails.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useMutateData } from '@/hooks/useMutateData'
 import { yupResolver } from '@hookform/resolvers/yup'
 import { useForm } from 'react-hook-form'
@@ -7,6 +7,15 @@ import { DefaultButton } from '../../component/Button'
 import { InputField, SelectField } from './FormField'
 import { AirtimePurchase } from '@/store/store'
 import { Formtitle } from './Formtitle'
+import { CustomModal, Modal } from '@/app/component/Modal'
+import { Header } from '../airtime/receipt/component/Header'
+import Image from 'next/image'
+import Brand from '../../asset/ajirobalogo.png'
+import {useRouter} from "next/navigation"
+import mtnicon from '../../asset/mtnicon.svg'
+import airtelicon from '../../asset/airtelicon.png'
+import gloicon from '../../asset/gloicon.png'
+import ninemobileicon from '../../asset/ninemobileicon.png'
 
 type AirtimeProps = {
   network: string
@@ -17,6 +26,16 @@ type AirtimeProps = {
 export const AirtimeDetails = () => {
   const setAirtimeDetails = AirtimePurchase(state => state.setAirtimeDetails)
   const setAirtimeStepper = AirtimePurchase(state => state.setAirtimeStepper)
+   const [printreceipt, setprintreceipt] = useState<boolean>(false);
+
+
+   const mobiledata = [
+  { id: 1, number: '08134538765', type: 'MTN', icon: mtnicon },
+  { id: 2, number: '65747533534', type: 'Airtel', icon: airtelicon },
+  { id: 3, number: '34324636267367', type: '9mobile', icon: ninemobileicon },
+  { id: 4, number: '08134538765', type: 'Glo', icon: gloicon },
+  // { id: 5, number: '08134538765', type: 'MTN', icon: '/path-to-mtn-icon.png' },
+];
 
   const {
     reset,
@@ -31,13 +50,26 @@ export const AirtimeDetails = () => {
     mode: 'all',
     resolver: yupResolver(Airtimeschema)
   })
-  const network = ['MTN', 'Airtel', 'Glo']
+
+
+
+
+  const network = ['MTN', 'Airtel', 'Glo', '9mobile', 'Etisalat', 'Smile', 'Virgin' ,  ]
 
 
   const sumbitForm = (data: AirtimeProps) => {
     // console.log('data=>', data)
     setAirtimeDetails(data)
     setAirtimeStepper(1)
+  }
+
+  const router =useRouter()
+
+
+    // Function to populate phone number when "Use" button is clicked
+  const handleUseClick = (number: string) => {
+    setValue('phone', number) // Set the 'phone' field value
+    setprintreceipt(false) // Close the modal
   }
 
   return (
@@ -79,7 +111,7 @@ export const AirtimeDetails = () => {
             placeholder='Amount'
 
           />
-          <p className='text-end text-[#f25e26] underline cursor-pointer text-sm font-Poppins'>Beneficiaries</p>
+          <p  onClick={() => setprintreceipt(!printreceipt)} className='text-end text-[#f25e26] underline cursor-pointer text-sm font-Poppins'>Beneficiaries</p>
 
           <div className='mb-6'>
             <DefaultButton
@@ -89,6 +121,50 @@ export const AirtimeDetails = () => {
               className='my-10 w-full bg-[#FCDFD4] p-3 rounded-lg '
             />
           </div>
+
+             <CustomModal isOpen={printreceipt}>
+
+            <section className='container flex flex-col '>
+      <div className='flex justify-between items-center'>
+        <Image src={Brand} alt='brand-logo' />
+        <h2 className=' font-Poppins text-[#2A2A2A] font-normal   lg:text-xl md:text-xl leading-3 capitalize'>{'Airtime Recharge' }</h2>
+      </div>
+      <p className="brand1 py-4 cursor-pointer font-Poppins text-sm underline"  onClick={()=> setprintreceipt(false)}>Back</p>
+    </section>
+
+
+
+
+     <div className="p-6 bg-gray-100 min-h-screen">
+      <div className="space-y-4">
+        {mobiledata?.map((item) => (
+          <div
+            key={item.id}
+            className="flex items-center justify-between bg-white p-4 rounded-lg shadow-sm hover:shadow-md transition-shadow"
+          >
+            {/* Icon and details */}
+            <div className="flex items-center">
+              <Image
+                src={item.icon}
+                alt={item.type}
+                className="w-10 h-10 rounded-full mr-4"
+              />
+              <div>
+                <p className="text-gray-900 font-semibold">{item.number}</p>
+                <p className="text-gray-500 text-sm">{item.type}</p>
+              </div>
+            </div>
+
+            {/* Action button */}
+            <button onClick={() => handleUseClick(item.number)}  className="px-4 py-2 bg-[#FCDFD4] text-[#2A2A2A] font-medium rounded-lg hover:bg-[#FCDFD4]">
+              Use
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+
+      </CustomModal>
         </form>
       </div>
     </div>
