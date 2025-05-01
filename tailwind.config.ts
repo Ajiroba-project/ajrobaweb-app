542d6dde608263539fbc81f0b03aabd08a92b36e
femakin <santosfemakin@yahoo.com>
2025-05-01 18:44:19 +0100

    rafffle image added and bug fixes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,7 +5,7 @@ import type { Config } from 'tailwindcss'
 import tailwindcssAnimate from 'tailwindcss-animate'
 
 const config = {
-  darkMode: false,
+  darkMode: 'class',
   content: [
     './pages/**/*.{ts,tsx}',
     './components/**/*.{ts,tsx}',
