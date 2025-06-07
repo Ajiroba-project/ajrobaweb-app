92d3941216d17cb59a0802a1525f3c6330cff3bf
femakin <santosfemakin@yahoo.com>
2025-06-07 19:50:46 +0100

    fixes

--- src/app/recharge/electricity/receipt/page.tsx
+++ src/app/recharge/electricity/receipt/page.tsx
@@ -14,7 +14,7 @@ import { error } from "console";
 import verify from "@/app/asset/verify.svg";
 import Cookies from "js-cookie";
 
-import { PDFDownloadLink } from "@react-pdf/renderer";
+import { BlobProviderParams, PDFDownloadLink } from "@react-pdf/renderer";
 import Receipt from "./receiptpdf/Receipt";
 import ReceiptPDF from "./receiptpdf/Receipt";
 
@@ -152,7 +152,7 @@ const WrappedPage = () => {
           document={<ReceiptPDF data={transdata?.data} message={transdata?.message} />}
           fileName={`receipt`}
         >
-          {({ blob, url, loading, error }) =>
+         {/*  {({ blob, url, loading, error }) =>
             loading ? (
               "Loading..."
             ) : (
@@ -166,7 +166,22 @@ const WrappedPage = () => {
                 </button>
               </div>
             )
-          }
+          } */}
+          {(({ loading }: BlobProviderParams) =>
+            loading ? (
+              <span>Loading...</span>
+            ) : (
+              <div>
+                <button
+                  type="button"
+                  onClick={close}
+                  className={`my-6 rounded-lg bg-[#FCDFD4] text-[#2A2A2A] p-4 px-10 hover:bg-[#F25E26] hover:text-white `}
+                >
+                  Download
+                </button>
+              </div>
+            )
+          ) as unknown as React.ReactNode}  
         </PDFDownloadLink>
       </section>
     </section>
